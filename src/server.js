306f748942a5b8ebc469bead0d63c28d4a7a5031
Purin (Trak) Prateepmanowong <purin.trak@gmail.com>
2025-05-22 00:03:49 -0400

    patch 2.2

--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
 import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import session from 'cookie-session';
+
 import tilesRouter from './routes/tiles.js';
+import spotifyRoutes from './routes/spotify.js';
+import authRoutes from './routes/auth.js';
+
 dotenv.config();
 
 if (!process.env.SPOTIFY_CLIENT_ID || !process.env.SPOTIFY_CLIENT_SECRET) {
   console.error('SPOTIFY credentials not loaded – check your .env file');
   process.exit(1);
 }
-
-import express  from 'express';
-import mongoose from 'mongoose';
-import cors     from 'cors';
-import session  from 'cookie-session';
-
-import spotifyRoutes from './routes/spotify.js';
-import authRoutes    from './routes/auth.js';
-
 
 const app = express();
 
@@ -31,17 +30,18 @@ app.use(cors({
   origin: [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'https://project-music-and-memories.onrender.com',
   ],
   credentials: true,
 }));
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/tiles', tilesRouter);
-app.use('/',    spotifyRoutes);
-app.use('/auth', authRoutes);
-app.use('/api',  authRoutes);
 
+
+app.use('/api/tiles', tilesRouter);
+app.use('/api/auth', authRoutes);
+app.use('/auth', authRoutes);
+app.use('/auth', spotifyRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✓ MongoDB connected'))
